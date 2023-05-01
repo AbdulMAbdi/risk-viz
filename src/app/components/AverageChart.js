7f6b4027221b84f8a80b41432f2c355523806852
AbdulMAbdi <51063017+AbdulMAbdi@users.noreply.github.com>
2023-05-01 03:57:10 -0400

    add legend to averages chart

--- src/app/components/AverageChart.js
+++ src/app/components/AverageChart.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -11,6 +11,12 @@ import {
 } from "chart.js";
 import { Bar } from "react-chartjs-2";
 import { useDataStore } from "../store/DataStore";
+import LegendToggleIcon from '@mui/icons-material/LegendToggle';
+import { blue } from "@mui/material/colors";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
 
 ChartJS.register(
   CategoryScale,
@@ -23,15 +29,27 @@ ChartJS.register(
 );
 
 export default function AverageChart() {
-  const [filteredData, factor, factorList, averagedData, actions] =
-    useDataStore((state) => [
+  const [filteredData, factor, averagedData, actions] = useDataStore(
+    (state) => [
       state.filteredData,
       state.factor,
-      state.factorList,
       state.averagedData,
       state.actions,
-    ]);
+    ]
+  );
+  const [infoOpen, setInfoOpen] = useState(false);
 
+  const style = {
+    position: "absolute",
+    top: "10.5%",
+    right: "2%",
+    width: 600,
+    bgcolor: "#dbf4f4",
+    border: "2px solid #000",
+    boxShadow: 24,
+    p: 4,
+    whiteSpace: "pre-line",
+  };
   const backgroundColors = {
     Earthquake: "rgba(209, 178, 111,0.75)", //
     "Extreme heat": "rgba(206, 32, 41,0.75)", //
@@ -44,6 +62,27 @@ export default function AverageChart() {
     "Extreme cold": "rgba(54, 139, 193,0.75)",
     "Sea level rise": "rgba(9, 88, 89,0.75)", //
   };
+
+  const legendItems = [];
+  for (const prop in backgroundColors) {
+    let item = (
+      <Grid container spacing={2}>
+        <Grid item xs={1}>
+          <Box
+            sx={{
+              width: 30,
+              height: 30,
+              bgcolor: backgroundColors[prop],
+            }}
+          ></Box>
+        </Grid>
+        <Grid item xs={11}>
+          <Typography id="modal-modal-description">{"- " + prop}</Typography>
+        </Grid>
+      </Grid>
+    );
+    legendItems.push(item);
+  }
   const options = {
     responsive: true,
     plugins: {
@@ -114,13 +153,53 @@ export default function AverageChart() {
   }, [filteredData]);
 
   return (
-    <Bar
-      class="group inline-block relative"
-      width="100vw"
-      height="40.75vh"
-      options={options}
-      data={barData}
-      plugins={[plugin]}
-    ></Bar>
+    <>
+      <div class="p-1">
+        <div class="group inline-block relative pl-40">
+          <button
+            class="bg-blue-400 text-gray-700 font-semibold py-2 px-4 rounded inline-flex items-center"
+            onClick={() => {
+              setInfoOpen(true);
+            }}
+          >
+            <LegendToggleIcon sx={{ color: blue[900] }} />
+          </button>
+          <Modal
+            open={infoOpen}
+            onClose={() => setInfoOpen(false)}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+          >
+            <Box sx={style}>
+              <Typography id="modal-modal-title" variant="h5" component="h2">
+                {"Average Risk Ratings Bar Chart"}
+              </Typography>
+              <Typography id="modal-modal-description">
+                {
+                  "This chart represents the average risk rating for all risk factors. The data being averaged can be filted with the drop down menus in the navigation bar"
+                }
+              </Typography>
+              <Typography
+                id="modal-modal-title"
+                variant="h6"
+                component="h2"
+                sx={{ mt: 2 }}
+              >
+                {"Risk Factors Legend"}
+              </Typography>
+              {legendItems}
+            </Box>
+          </Modal>
+        </div>
+      </div>
+      <Bar
+        class="group inline-block relative"
+        width="100vw"
+        height="40.75vh"
+        options={options}
+        data={barData}
+        plugins={[plugin]}
+      ></Bar>
+    </>
   );
 }
